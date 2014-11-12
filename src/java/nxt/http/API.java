b4d576867849074dc5ddb9d140f7494bf7d9e475
Uray Meiviar <uraymeiviar@gmail.com>
2014-11-13 01:29:26 +0700

    compile error fix

--- src/java/nxt/http/API.java
+++ src/java/nxt/http/API.java
@@ -76,7 +76,7 @@ public final class API {
                 sslContextFactory.setIncludeCipherSuites("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
                                                          "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
                                                          "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384",
-                                                         "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA");
+                                                         "TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA",
                                                          "TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA",
                                                          "TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA",
                                                          "TLS_RSA_WITH_AES_128_CBC_SHA",
